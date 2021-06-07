ce335541afab80e55740cea361836755a88e63b8
Filip Bosnic <fbosnic@fer.hr>
2021-06-07 11:48:22 +0200

    Switched to relative path for config.cfg

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,10 @@
 import configparser
-
+import os.path
 
 DEFAULT_DATASET_SIZES = [25, 50, 100, 150, 200]
 DEFAULT_EXAMPLES_PER_SIZE_IN_DATASET = 5000
 
-CONFIG_FILE_PATH = "config.cfg"
+CONFIG_FILE_PATH = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "config.cfg"))
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE_PATH)
 MODEL_WEIGHTS_FOLDER = config["MODELS"]["MODEL_WEIGHTS_FOLDER"]
