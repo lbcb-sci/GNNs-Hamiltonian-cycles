940e6ca5cb1e2773d2680f074ecd0f796ddd3a37
Filip Bosnic <fbosnic@fer.hr>
2021-09-15 16:07:00 +0200

    Started performance measurements

---
+++ src/Development_code/performance_measures/layer_compute_speed.py
@@ -0,0 +1,86 @@
+import time
+import itertools
+
+import torch
+import torch_geometric as torch_g
+import torch_scatter
+
+from src.DatasetBuilder import ErdosRenyiInMemoryDataset, ErdosRenyiGenerator
+from src.Models import GatedGCNEmbedAndProcess, HamiltonianCycleFinder, EncodeProcessDecodeAlgorithm
+from src.ExactSolvers import ConcordeHamiltonSolver
+
+
+def time_operation(operation, dataset):
+    total_time = 0
+    nr_examples = 0
+    result = None
+    for d in dataset:
+        start_time = time.time()
+        result = operation(d)
+        end_time = time.time()
+        total_time += end_time - start_time
+        nr_examples += 1
+    return total_time / nr_examples, result
+
+
+def time_operation_on_ER(operation, sizes=None, ham_prob=0.8, nr_examples_per_size=10):
+    if sizes is None:
+        sizes = [25, 100, 400, 1600, 5000]
+    timings = []
+    for s in sizes:
+        generator = ErdosRenyiGenerator(s, ham_prob)
+        t, _ = time_operation(operation, itertools.islice(generator, nr_examples_per_size))
+        timings.append(t)
+    return timings
+
+
+def operation_forward_step(model: HamiltonianCycleFinder, d: torch_g.data.Data):
+    model.init_graph(d)
+    model.prepare_for_first_step(d, torch.tensor([0], device=d.edge_index.device))
+    return model.next_step_prob_masked_over_neighbors(d)
+
+
+def profile_EPD(model: EncodeProcessDecodeAlgorithm, d):
+    times = {}
+    d = torch_g.data.Batch.from_data_list([d])
+    times["data_init"], _ = time_operation(lambda a: model.init_graph(a), [d])
+    times["data_prep"], _ = time_operation(lambda a: model.prepare_for_first_step(a, [0]), [d])
+    times["pure_logits"], l = time_operation(lambda a: model.next_step_logits(d), [d])
+    times["neighbor_logits"], n = time_operation(lambda a: model._mask_neighbor_logits(l, a), [d])
+    times["probs_computation"], p = time_operation(lambda a: torch_scatter.scatter_softmax(n, a.batch), [d])
+
+    def greedy_choice(p):
+        choice = torch.argmax(
+            torch.isclose(p, torch.max(p, dim=-1)[0][..., None]) * (p + torch.randperm(p.shape[-1])[None, ...]), dim=-1)
+        choice = choice + torch_scatter.scatter_sum(d.batch, d.batch, dim_size=d.num_graphs)
+        return choice
+
+    times["selection"], choice = time_operation(lambda d: greedy_choice(p), [d])
+
+    return times
+
+
+if __name__ == '__main__':
+    HamS_model = EncodeProcessDecodeAlgorithm(True, processor_depth=5, hidden_dim=32)
+    generator = ErdosRenyiGenerator(10_000, 0.8)
+    times = profile_EPD(HamS_model, next(iter(generator)))
+    print(times)
+    total = sum(times.values())
+    perc = {k: v/total for k, v in times.items()}
+    print(perc)
+
+    d = next(iter(generator))
+    d = torch_g.data.Batch.from_data_list([d])
+    full, _ = time_operation(lambda a: operation_forward_step(HamS_model, a), [d])
+    print(full, total)
+    exit(-5)
+
+    sizes = [25, 50, 100, 200]
+    concorde_solver = ConcordeHamiltonSolver()
+    timings = time_operation_on_ER(lambda d: concorde_solver.solve(d), sizes)
+    print(timings)
+    exit()
+    timings = time_operation_on_ER(
+        lambda d: HamS_model.batch_run_greedy_neighbor(torch_g.data.Batch.from_data_list([d])),
+        sizes=sizes)
+    print(timings)
