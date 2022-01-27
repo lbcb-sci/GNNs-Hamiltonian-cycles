d6df2a340ff743e156a2da44d0c20b07f0e0c9bf
Filip Bosnic <fbosnic@fer.hr>
2022-01-27 11:12:31 +0000

    Added wandb logger

--- tst/test_lightning_working.py
+++ tst/test_lightning_working.py
@@ -1,6 +1,8 @@
+from asyncio.log import logger
 import torch
 from torch.utils.data import DataLoader
 import pytorch_lightning as torch_lightning
+from pytorch_lightning.loggers import WandbLogger
 
 import src.Models as Models
 from src.data.InMemoryDataset import ErdosRenyiInMemoryDataset
@@ -14,8 +16,9 @@ if __name__ == "__main__":
     dataset = GraphGeneratingDataset(generator)
     dataloader = GraphDataLoader(dataset, batch_size=10)
     model = Models.EncodeProcessDecodeAlgorithm(False)
+    wandb_logger = WandbLogger(project="gnns-Hamiltonian-cycles")
     torch.set_num_threads(16)
-    trainer = torch_lightning.Trainer(max_epochs=1, num_sanity_val_steps=2, val_check_interval=10)
+    trainer = torch_lightning.Trainer(max_epochs=1, num_sanity_val_steps=2, val_check_interval=10, logger=wandb_logger)
     datamodule = ArtificialCycleDataModule()
     trainer.fit(model=model, datamodule=datamodule)
     trainer.test(model, test_dataloaders=datamodule.test_dataloader())
