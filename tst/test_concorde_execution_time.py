086358b7dc34b8e9755fd14b0e9b7f50af6e6bd6
Filip Bosnic <fbosnic@fer.hr>
2022-01-28 13:09:15 +0000

    Concorde execution w.r.t. ham_existence probability

--- tst/test_concorde_execution_time.py
+++ tst/test_concorde_execution_time.py
@@ -6,16 +6,16 @@ import pandas
 import time
 from pathlib import Path
 
-from src.GraphGenerators import ErdosRenyiGenerator
+from src.data.GraphGenerators import ErdosRenyiGenerator
 from src.ExactSolvers import ConcordeHamiltonSolver
 
 
 def test_concorde_execution_time():
-    graph_sizes = [int((1.5)**k) for k in range(26, 35)]
-    examples_per_size = 3
-    ham_existence_prob = 0.8
+    graph_sizes = [int((1.5)**k) for k in range(9, 29)]
+    ham_existence_prob = [0.1, 0.3, 0.5, 0.7, 0.95]
+    examples_per_size = 5
     timestamp = int(time.time())
-    
+
     print(f"Checking concorde execution time on Erdos-Renyi graphs with {ham_existence_prob} asysmptotic probability of being Hamiltonian."
           f"Timing execution {examples_per_size} times per each graph_size in {graph_sizes}")
     if platform.system() == "Linux":
@@ -24,12 +24,12 @@ def test_concorde_execution_time():
         processor_model_name = "unknown"
     print(f"Processor model: {processor_model_name} (Concorde is a single-thread program)")
     concorde_solver = ConcordeHamiltonSolver(working_subdir="test_execution_time")
-    
+
     _sizes_column = []
     _real_time_column = []
     _user_time_column = []
     _is_hamiltonian_column = []
-    for size in tqdm(graph_sizes):
+    for size, ham_existence_prob in tqdm(list(itertools.product(graph_sizes, ham_existence_prob))):
         generator = ErdosRenyiGenerator(size, ham_existence_prob)
         for graph in tqdm(itertools.islice(generator, examples_per_size), leave=False, total=examples_per_size):
             solution, real_time, user_time = concorde_solver.time_execution(graph)
@@ -37,13 +37,13 @@ def test_concorde_execution_time():
             _real_time_column.append(real_time)
             _user_time_column.append(user_time)
             _is_hamiltonian_column.append(len(solution) == (size + 1))
-            
+
     df_times = pandas.DataFrame({"graph_size": _sizes_column, "real_execution_time": _real_time_column,
                                  "user_execution_time": _user_time_column, "is_hamiltonian": _is_hamiltonian_column,
                                  "timestamp": timestamp, "HC_existence_probability": ham_existence_prob})
     return df_times
 
-    
+
 if __name__ == "__main__":
     df_path = Path("tests/concorde_execution_times.csv")
     df_new = test_concorde_execution_time()
