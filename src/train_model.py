895222bc5bf314d3e9b2f4b4d529e1e1082eb038
Filip Bosnic <fbosnic@fer.hr>
2022-02-17 13:50:10 +0000

    Correctly logging wandb run data

--- src/train_model.py
+++ src/train_model.py
@@ -13,19 +13,19 @@ def train_model(model_class, datamodule_class, model_checkpoint=None, model_hype
                 is_log_offline=False, wandb_project=constants.WEIGHTS_AND_BIASES_PROJECT, run_name=None, is_log_model=True):
     if run_name is None:
         datetime_now = datetime.datetime.now()
-        run_name = f"{model_class}_{datetime_now.strftime('%Y%m%d%h')[2:]}"
+        run_name = f"{model_class.__name__.split('.')[-1][:10]}_{datetime_now.strftime('%Y%m%d%H%M')[2:]}"
     wandb_logger = lightning_loggers.WandbLogger(name=run_name, project=wandb_project, offline=is_log_offline, log_model=is_log_model)
-    wandb_logger.experiment.log({"is_started_from_checkpoint": model_checkpoint is not None,
-                                 "checkpoint": model_checkpoint})
+    wandb_logger.experiment.summary["is_started_from_checkpoint"] = model_checkpoint is not None
+    wandb_logger.experiment.summary["checkpoint"] = str(model_checkpoint)
 
     if model_checkpoint is None:
         model = model_class(**model_hyperparams)
     else:
         model = model_class.load_from_checkpoint(model_checkpoint)
-    wandb_logger.experiment.log({"description": model.description()})
+    wandb_logger.experiment.summary["description"] = model.description()
 
     datamodule_hyperparams.update({
-        DataModules.SIMULATION_MODULE_VARIABLE_NAME: model
+        DataModules.SIMULATION_MODULE_VARIABLE_NAME: model,
     })
     datamodule = datamodule_class(**datamodule_hyperparams)
 
@@ -44,7 +44,6 @@ def train_model(model_class, datamodule_class, model_checkpoint=None, model_hype
 
     wandb_logger.finalize()
 
-
 if __name__== "__main__":
     model_class = Models.GatedGCNEmbedAndProcess
     model_hyperparams = {"processor_depth": 3}
@@ -52,6 +51,8 @@ if __name__== "__main__":
     datamodule_class = DataModules.ReinforcementErdosRenyiDataModule
     datamodule_hyperparams = {}
 
-    trainer_hyperparams = {"max_epochs": 10, "num_sanity_val_steps": 2}
+    trainer_hyperparams = {"max_epochs": 10, "num_sanity_val_steps": 2, "log_every_n_steps": 20}
+    datamodule_hyperparams = {"train_virtual_epoch_size": 1000}
 
-    train_model(model_class, datamodule_class, model_hyperparams=model_hyperparams, trainer_hyperparams=trainer_hyperparams)
+    train_model(model_class, datamodule_class, model_hyperparams=model_hyperparams, trainer_hyperparams=trainer_hyperparams,
+                datamodule_hyperparams=datamodule_hyperparams)
