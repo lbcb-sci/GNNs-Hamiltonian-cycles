273511bfe333ae1b2ba29405e7547e6db058d08c
Filip Bosnic <fbosnic@fer.hr>
2022-02-18 11:02:19 +0000

    Trying to manually test models and update to wandb

--- src/test_model.py
+++ src/test_model.py
@@ -1,5 +1,6 @@
 import copy
 
+import torch
 from pytorch_lightning import Trainer
 import pytorch_lightning
 
@@ -8,21 +9,35 @@ import src.Models as Models
 import src.data.GraphDataset as GraphDataset
 from src.data.InMemoryDataset import ErdosRenyiInMemoryDataset
 
-def test_model_unified(model):
-    dataset = ErdosRenyiInMemoryDataset(constants.EVALUATION_DATA_FOLDERS)
-    sizes = list(set(graph_item.graph.num_nodes for graph_item in dataset))
-    test_dataloaders = []
-    for size in sizes:
-        subset_dataset = copy.deepcopy(dataset)
-        subset_dataset.data_list = \
-            [data_item for data_item in dataset.data_list if data_item[ErdosRenyiInMemoryDataset.NUM_NODES_TAG] == size]
-        test_dataloaders.append(GraphDataset.GraphDataLoader(subset_dataset))
+def test_model_unified(model_class, model_hyperparams = {}, model_checkpoint=None, wandb_run_id=None, torch_num_threads=32):
+    torch.set_num_threads(torch_num_threads)
+    if model_checkpoint is None:
+        model = model_class(model_hyperparams)
+    else:
+        model = model_class.load_from_checkpoint()
 
-    results = {}
-    trainer = pytorch_lightning.Trainer()
-    for size, test_dataloader in zip(size, test_dataloaders):
-         results[size] = trainer.test(model, dataloaders=test_dataloaders[1])
-    return results
+    timestamp = time.time()
+    # TODO add wandb logging connection
+
+    df = EvaluationScores.accuracy_scores_on_saved_data(
+        [HamS_model, HamR_model, hybrid_ham_heuristics, least_degree_first, concorde_solver],
+        ["HamS", "HamR", "HybridHam", "Least_degree_first", "Concorde"], nr_graphs_per_size=10_000)
+    df["timestamp"] = timestamp
+    return df
+    # dataset = ErdosRenyiInMemoryDataset(constants.EVALUATION_DATA_FOLDERS)
+    # sizes = list(set(graph_item.graph.num_nodes for graph_item in dataset))
+    # test_dataloaders = []
+    # for size in sizes:
+    #     subset_dataset = copy.deepcopy(dataset)
+    #     subset_dataset.data_list = \
+    #         [data_item for data_item in dataset.data_list if data_item[ErdosRenyiInMemoryDataset.NUM_NODES_TAG] == size]
+    #     test_dataloaders.append(GraphDataset.GraphDataLoader(subset_dataset))
+
+    # results = {}
+    # trainer = pytorch_lightning.Trainer()
+    # for size, test_dataloader in zip(size, test_dataloaders):
+    #      results[size] = trainer.test(model, dataloaders=test_dataloaders[1])
+    # return results
 
 
 if __name__ == "__main__":
