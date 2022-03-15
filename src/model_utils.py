df09a6441ce7b35c4da3ab858134b18f4eec9320
Filip Bosnic <fbosnic@fer.hr>
2022-03-15 12:47:19 +0000

    Setting up model testing

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,11 +1,20 @@
 import datetime
 import itertools
+import time
+from typing import Mapping
+from numpy import size
+
 import torch
 import pytorch_lightning as torch_lightning
 import pytorch_lightning.loggers as lightning_loggers
+import wandb
 
+from src.Evaluation import EvaluationScores
+from src.HamiltonSolver import HamiltonSolver
+from src.data.GraphDataset import GraphDataLoader, GraphExample, GraphGeneratingDataset
 import src.data.DataModules as DataModules
 import src.constants as constants
+from src.data.InMemoryDataset import ErdosRenyiInMemoryDataset
 
 
 def train_model(model_class, datamodule_class, model_checkpoint=None, model_hyperparams={}, datamodule_hyperparams={},
@@ -46,8 +55,57 @@ def train_model(model_class, datamodule_class, model_checkpoint=None, model_hype
     trainer.fit(**train_parameters)
 
     trainer.test(datamodule=datamodule)
-
     wandb_logger.finalize("Success")
+
+
+def test_on_saved_data(model_class: HamiltonSolver, wandb_run=None, model_hyperparams={}, model_checkpoint=None):
+    if model_checkpoint is None:
+        model = model_class(model_hyperparams)
+    else:
+        model = model_class.load_from_checkpoint()
+    df_testing_results = EvaluationScores.accuracy_scores_on_saved_data([model], ["model"])
+
+    unified_test_tag = "unified_test"
+
+    if wandb_run is not None:
+        for row_index, row in df_testing_results.iterrows():
+            for accuracy_tag in [EvaluationScores.ACCURACY_SCORE_TAGS.perc_hamilton_found, EvaluationScores.ACCURACY_SCORE_TAGS.perc_long_cycles_found,
+                                EvaluationScores.ACCURACY_SCORE_TAGS.perc_full_walks_found, EvaluationScores.ACCURACY_SCORE_TAGS.perc_long_walks_found]:
+                wandb_run.log({f"{unified_test_tag}/size": row["size"], f"{unified_test_tag}/{accuracy_tag}": row[accuracy_tag]})
+    return df_testing_results
+
+
+def test_model_on_saved_data(model_class, model_hyperparams = {}, model_checkpoint=None, wandb_run_id=None):
+    dataset = ErdosRenyiInMemoryDataset([constants.EVALUATION_DATA_FOLDERS])
+
+    sizes = set(graph_example.graph.num_nodes for graph_example in dataset)
+    name_to_datasets = {
+        size: (graph_example for graph_example in dataset if graph_example.graph.num_nodes == size)
+        for size in sizes
+    }
+
+    return 0
+    timestamp = time.time()
+    # TODO add wandb logging connection
+
+    df = EvaluationScores.accuracy_scores_on_saved_data(
+        [HamS_model, HamR_model, hybrid_ham_heuristics, least_degree_first, concorde_solver],
+        ["HamS", "HamR", "HybridHam", "Least_degree_first", "Concorde"], nr_graphs_per_size=10_000)
+    df["timestamp"] = timestamp
+    dataset = ErdosRenyiInMemoryDataset(constants.EVALUATION_DATA_FOLDERS)
+    sizes = list(set(graph_item.graph.num_nodes for graph_item in dataset))
+    test_dataloaders = []
+    for size in sizes:
+        subset_dataset = copy.deepcopy(dataset)
+        subset_dataset.data_list = \
+            [data_item for data_item in dataset.data_list if data_item[ErdosRenyiInMemoryDataset.NUM_NODES_TAG] == size]
+        test_dataloaders.append(GraphDataset.GraphDataLoader(subset_dataset))
+
+    results = {}
+    trainer = pytorch_lightning.Trainer()
+    for size, test_dataloader in zip(size, test_dataloaders):
+         results[size] = trainer.test(model, dataloaders=test_dataloaders[1])
+    return results
 
 
 class ModelTrainRequest:
--- src/test_model.py
+++ src/test_model.py
@@ -8,41 +8,21 @@ import src.constants as constants
 import src.Models as Models
 import src.data.GraphDataset as GraphDataset
 from src.data.InMemoryDataset import ErdosRenyiInMemoryDataset
-
-def test_model_unified(model_class, model_hyperparams = {}, model_checkpoint=None, wandb_run_id=None, torch_num_threads=32):
-    torch.set_num_threads(torch_num_threads)
-    if model_checkpoint is None:
-        model = model_class(model_hyperparams)
-    else:
-        model = model_class.load_from_checkpoint()
-
-    timestamp = time.time()
-    # TODO add wandb logging connection
-
-    df = EvaluationScores.accuracy_scores_on_saved_data(
-        [HamS_model, HamR_model, hybrid_ham_heuristics, least_degree_first, concorde_solver],
-        ["HamS", "HamR", "HybridHam", "Least_degree_first", "Concorde"], nr_graphs_per_size=10_000)
-    df["timestamp"] = timestamp
-    return df
-    # dataset = ErdosRenyiInMemoryDataset(constants.EVALUATION_DATA_FOLDERS)
-    # sizes = list(set(graph_item.graph.num_nodes for graph_item in dataset))
-    # test_dataloaders = []
-    # for size in sizes:
-    #     subset_dataset = copy.deepcopy(dataset)
-    #     subset_dataset.data_list = \
-    #         [data_item for data_item in dataset.data_list if data_item[ErdosRenyiInMemoryDataset.NUM_NODES_TAG] == size]
-    #     test_dataloaders.append(GraphDataset.GraphDataLoader(subset_dataset))
-
-    # results = {}
-    # trainer = pytorch_lightning.Trainer()
-    # for size, test_dataloader in zip(size, test_dataloaders):
-    #      results[size] = trainer.test(model, dataloaders=test_dataloaders[1])
-    # return results
-
+import src.model_utils as model_utils
 
 if __name__ == "__main__":
+    args =
+    wandb_kwargs = {"project": wandb_project, "resume": True}
+    if wandb_id is not None:
+        wandb_kwargs["id"] = wandb_id
+    wandb.init(**wandb_kwargs)
     model_class = Models.EncodeProcessDecodeAlgorithm
     model_checkpoint = None
-    model = model_class.load_from_checkpoint(model_checkpoint)
-    results = test_model_unified(model)
+    model = Models.EncodeProcessDecodeAlgorithm()
+    results = model_utils.test_on_saved_data(model)
+
+    # model = model_class.load_from_checkpoint(model_checkpoint)
+
+    exit(-1)
+    results = model_utils.test_model_on_saved_data(model_class, )
     print(results)
