fa4740265fa716e8e7df8d23aebae39ba9287392
Filip Bosnic <fbosnic@fer.hr>
2022-03-16 12:38:23 +0000

    Updated model list

--- src/models_list.py
+++ src/models_list.py
@@ -35,8 +35,19 @@ train_request_HamS_model = model_utils.ModelTrainRequest(
 train_request_HamS_mse_loss = copy.deepcopy(train_request_HamS_model)
 train_request_HamS_mse_loss.arguments["model_hyperparams"].update({"loss_type": "mse"})
 
-train_request_HamS_expanded = copy.deepcopy(train_request_HamS_model)
-train_request_HamS_expanded.arguments["model_hyperparams"].update({"processor_depth": 10})
 
-train_request_HamS_larger_graphs = copy.deepcopy(train_request_HamS_model)
-train_request_HamS_larger_graphs.arguments["datamodule_hyperparams"].update({"train_graph_size": 50, "val_graph_size": 50})
+train_request_HamS_depth_3 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_depth_3.arguments["model_hyperparams"].update({"processor_depth": 3})
+
+train_request_HamS_depth_10 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_depth_10.arguments["model_hyperparams"].update({"processor_depth": 10})
+
+train_request_HamS_depth_15 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_depth_15.arguments["model_hyperparams"].update({"processor_depth": 15})
+
+
+train_request_HamS_graphs_50 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_graphs_50.arguments["datamodule_hyperparams"].update({"train_graph_size": 50, "val_graph_size": 50})
+
+train_request_HamS_graphs_100 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_graphs_100.arguments["datamodule_hyperparams"].update({"train_graph_size": 50, "val_graph_size": 30})
