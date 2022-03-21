5eb07ba73537e39385d1bdab0cc4f4934ea78294
Filip Bosnic <fbosnic@fer.hr>
2022-03-21 11:58:33 +0000

    Bugfix - log function exist in the pytorchlightning module not trainer

--- src/callbacks.py
+++ src/callbacks.py
@@ -7,7 +7,7 @@ def _measure_max_logits_p_norm(pl_trainer, pl_module, p=2):
         norms = []
         for l in pl_module.logits_per_step:
             norms.append(torch.linalg.vector_norm(l, p))
-        pl_trainer.log(f"max_logits_l{p}_norm", torch.stack(norms).max())
+        pl_module.log(f"max_logits_l{p}_norm", torch.stack(norms).max())
 
 callback_max_logits_2_norm = LambdaCallback(
     on_after_backward=_measure_max_logits_p_norm
@@ -17,7 +17,7 @@ def _measure_max_weights_p_norm(pl_trainer, pl_module, p=2):
     norms = []
     for name, param in pl_module.named_parameters():
         norms.append(torch.linalg.vector_norm(param, p))
-    pl_trainer.log(f"max_l2_of_all_layers", torch.stack(norms).max())
+    pl_module.log(f"max_l2_of_all_layers", torch.stack(norms).max())
 
 
 callback_max_weights_2_norm = LambdaCallback(
