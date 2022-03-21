991daa4b456b6cbbe5e90e679c061a0d983cbecb
Filip Bosnic <fbosnic@fer.hr>
2022-03-21 11:49:56 +0000

    Bugfix - using torch.linalg.vactor_norm instead

--- src/callbacks.py
+++ src/callbacks.py
@@ -1,5 +1,4 @@
 import torch
-from torch.nn import functional as F
 from pytorch_lightning.callbacks import LambdaCallback
 
 
@@ -7,7 +6,7 @@ def _measure_max_logits_p_norm(pl_trainer, pl_module, p=2):
     if "logits_per_step" in vars(pl_module):
         norms = []
         for l in pl_module.logits_per_step:
-            norms.append(F.normalize(l, p))
+            norms.append(torch.linalg.vector_norm(l, p))
         pl_trainer.log(f"max_logits_l{p}_norm", torch.stack(norms).max())
 
 callback_max_logits_2_norm = LambdaCallback(
@@ -17,7 +16,7 @@ callback_max_logits_2_norm = LambdaCallback(
 def _measure_max_weights_p_norm(pl_trainer, pl_module, p=2):
     norms = []
     for name, param in pl_module.named_parameters():
-        norms.append(F.normalize(param, p))
+        norms.append(torch.linalg.vector_norm(param, p))
     pl_trainer.log(f"max_l2_of_all_layers", torch.stack(norms).max())
 
 
