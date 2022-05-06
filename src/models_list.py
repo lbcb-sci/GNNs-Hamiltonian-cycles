6f011ba480ace89870ed5c56c62800d5a3b42722
Filip Bosnic <fbosnic@fer.hr>
2022-05-06 10:58:53 +0000

    A couple of new models

--- src/models_list.py
+++ src/models_list.py
@@ -8,7 +8,7 @@ import src.data.DataModules as DataModules
 import src.model_utils as model_utils
 import src.callbacks as my_callbacks
 
-train_request_HamS_model = model_utils.ModelTrainRequest(
+train_request_HamS = model_utils.ModelTrainRequest(
     model_class = Models.EncodeProcessDecodeAlgorithm,
     datamodule_class = DataModules.ArtificialCycleWithDoubleEvaluationDataModule,
     model_checkpoint = None,
@@ -38,7 +38,7 @@ train_request_HamS_model = model_utils.ModelTrainRequest(
         "train_batch_size": 8,
         "val_batch_size": 8,
         "train_graph_size": 25,
-        "train_expected_noise_edges_per_node": 3,
+        "train_expected_noise_edges_per_node": 2,
         "val_hamiltonian_existence_probability": 0.8,
     },
     model_checkpoint_hyperparams = {
@@ -50,45 +50,45 @@ lr_callbacks = [LearningRateMonitor(logging_interval="step")]
 norm_monitoring_callbacks = [my_callbacks.create_lp_callback(target_type, p_norm) for target_type
        in ["max_lp_logits", "max_lp_weights", "max_lp_gradients", "flat_lp_gradients"] for p_norm in [2, 5]]
 checkpoint_callbacks = [ModelCheckpoint(save_top_k=3, save_last=True, monitor="val/ER/hamiltonian_cycle")]
-train_request_HamS_model.arguments["trainer_hyperparams"]["callbacks"] = lr_callbacks + norm_monitoring_callbacks + checkpoint_callbacks
+train_request_HamS.arguments["trainer_hyperparams"]["callbacks"] = lr_callbacks + norm_monitoring_callbacks + checkpoint_callbacks
 
 
-train_request_HamS_quick = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_quick = copy.deepcopy(train_request_HamS)
 train_request_HamS_quick.arguments["trainer_hyperparams"].update({"max_epochs": 50})
 
-train_request_HamS_mse_loss = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_mse_loss = copy.deepcopy(train_request_HamS)
 train_request_HamS_mse_loss.arguments["model_hyperparams"].update({"loss_type": "mse"})
 
 
-train_request_HamS_depth_3 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_depth_3 = copy.deepcopy(train_request_HamS)
 train_request_HamS_depth_3.arguments["model_hyperparams"].update({"processor_depth": 3})
 
-train_request_HamS_depth_7 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_depth_7 = copy.deepcopy(train_request_HamS)
 train_request_HamS_depth_7.arguments["model_hyperparams"].update({"processor_depth": 7})
 
-train_request_HamS_depth_15 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_depth_15 = copy.deepcopy(train_request_HamS)
 train_request_HamS_depth_15.arguments["model_hyperparams"].update({"processor_depth": 15})
 
 
-train_request_HamS_graphs_50 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_graphs_50 = copy.deepcopy(train_request_HamS)
 train_request_HamS_graphs_50.arguments["datamodule_hyperparams"].update({"train_graph_size": 50, "val_graph_size": 50})
 
-train_request_HamS_graphs_100 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_graphs_100 = copy.deepcopy(train_request_HamS)
 train_request_HamS_graphs_100.arguments["datamodule_hyperparams"].update({"train_graph_size": 100, "val_graph_size": 100})
 
-train_request_HamS_graphs_200 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_graphs_200 = copy.deepcopy(train_request_HamS)
 train_request_HamS_graphs_200.arguments["datamodule_hyperparams"].update({"train_graph_size": 200, "val_graph_size": 200})
 
 
-train_request_HamS_ER_exact_solver = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_ER_exact_solver = copy.deepcopy(train_request_HamS)
 train_request_HamS_ER_exact_solver.arguments["datamodule_class"] = DataModules.SolvedErdosRenyiDataModule
 train_request_HamS_ER_exact_solver.arguments["datamodule_hyperparams"].update({"train_hamilton_existence_probability": 0.8})
 del train_request_HamS_ER_exact_solver.arguments["datamodule_hyperparams"]["train_expected_noise_edges_per_node"]
 
-train_request_HamS_grad_clipping = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_grad_clipping = copy.deepcopy(train_request_HamS)
 train_request_HamS_grad_clipping.arguments["trainer_hyperparams"]["gradient_clip_val"] = 1
 
-train_request_HamS_custom_lr = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_custom_lr = copy.deepcopy(train_request_HamS)
 train_request_HamS_custom_lr.arguments["model_hyperparams"]["starting_learning_rate"] = 1e-5
 
 train_request_HamS_automatic_lr = copy.deepcopy(train_request_HamS_grad_clipping)
@@ -104,14 +104,17 @@ train_request_HamS_cosine_annealing.arguments["model_hyperparams"]["lr_scheduler
     "eta_min": 1e-6,
 }
 
-train_request_HamS_different_artificial_graphs_4 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_different_artificial_graphs_4 = copy.deepcopy(train_request_HamS)
 train_request_HamS_different_artificial_graphs_4.arguments["datamodule_hyperparams"]["train_expected_noise_edges_per_node"] = 4
 
-train_request_HamS_different_artificial_graphs_2 = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_different_artificial_graphs_2 = copy.deepcopy(train_request_HamS)
 train_request_HamS_different_artificial_graphs_2.arguments["datamodule_hyperparams"]["train_expected_noise_edges_per_node"] = 2
 
-train_request_HamS_mse = copy.deepcopy(train_request_HamS_model)
+train_request_HamS_mse = copy.deepcopy(train_request_HamS)
 train_request_HamS_mse.arguments["model_hyperparams"]["loss_type"] = "mse"
 
 train_request_HamS_mse_large = copy.deepcopy(train_request_HamS_mse)
 train_request_HamS_mse_large.arguments["datamodule_hyperparams"].update({"train_graph_size": 200, "val_graph_size": 200})
+
+train_request_HamS_mse_large_faster = copy.deepcopy(train_request_HamS_mse_large)
+train_request_HamS_mse_large_faster.arguments["model_hyperparams"]["starting_learning_rate"] *= 100
