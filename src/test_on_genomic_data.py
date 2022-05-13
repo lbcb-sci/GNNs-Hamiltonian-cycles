ba1dd4a44766d3508cc565ece47716adafa9b501
Filip Bosnic <fbosnic@fer.hr>
2022-05-13 12:58:16 +0000

    Reworked testing on genomic data

--- src/test_on_genomic_data.py
+++ src/test_on_genomic_data.py
@@ -34,10 +34,12 @@ def compute_genomics_statistic(eval: dict):
     df["coverage"] = df["length"] / df["graph_size"]
     return {
         "dataset_size": len(df.index),
-        "genome_coverage": df["coverage"].mean(),
+        "graph_coverage": df["coverage"].mean(),
         "perc_cycles": df["is_cycle"].mean(),
         "perc_ham_cycles": (df["is_cycle"] & (df["graph_size"] == df["length"])).mean(),
-        "cycles_genome_coverage": df[df["is_cycle"]]["coverage"].mean(),
+        "cycles_graph_coverage": df[df["is_cycle"]]["coverage"].mean(),
+        "perc_incorrect": (df["nr_incorrect_edges"] > 0).mean(),
+        "avg_incorrect_edges": df["nr_incorrect_edges"].mean()
     }
 
 
@@ -49,20 +51,30 @@ def test_on_genomic_dataset(model: HamiltonSolver, dataset_summary_file=None):
     return stats, eval
 
 def test_wandb_model_on_genomic_data(wandb_id: str, model:HamiltonSolver=None, dataset_summary_file=None, wandb_project=constants.WEIGHTS_AND_BIASES_PROJECT):
+    stats, eval = None, None
     try:
         wandb_run = wandb.init(project=wandb_project, id=wandb_id, resume=True)
         if model is None:
             model = model_utils.create_model_for_wandb_run(wandb_run)
-        stats, _ = test_on_genomic_dataset(model, dataset_summary_file)
-        for key, value in stats:
-            # wandb_run.log(f"genome_stats/{key}", value)
-            print(f"genome_stats/{key}", value)
-    except Exception as ex:
-        wandb_run.finish()
-        raise ex
+        stats, eval = test_on_genomic_dataset(model, dataset_summary_file)
+        wandb_run.log({f"string_graphs/{key}": value for key, value in stats.items()})
+    finally:
+        if wandb_run is not None:
+            wandb_run.finish()
+    return stats, eval
 
 
 if __name__ == "__main__":
+    from src.Development_code.Heuristics import HybridHam
     concorde_solver = ConcordeHamiltonSolver()
-    stats, eval = test_wandb_model_on_genomic_data("", concorde_solver)
-    print(stats)
+    hybrid_ham = HybridHam()
+
+    run_id = "s9ket5ka"
+
+    wandb_run = model_utils.reconnect_to_wandb_run(run_id)
+    hams = model_utils.create_model_for_wandb_run(wandb_run)
+    wandb_run.finish()
+
+    stats, eval = test_wandb_model_on_genomic_data(run_id, hams)
+    for key, value in stats.items():
+        print(f"{key}", value)
