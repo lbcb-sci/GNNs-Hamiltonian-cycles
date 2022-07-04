70bce37e3d26e959266f051747c6d8e98e06b0f3
Filip Bosnic <fbosnic@fer.hr>
2022-07-04 16:13:51 +0000

    More detailed output in dataset creation script

--- terminal_scripts/generate_data.py
+++ terminal_scripts/generate_data.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     sizes = DEFAULT_DATASET_SIZES
     nr_examples = DEFAULT_EXAMPLES_PER_SIZE_IN_DATASET
     print(f"Crating dataset of Erdos-Renyi graphs of sizes {sizes} with {DEFAULT_EXAMPLES_PER_SIZE_IN_DATASET}"
-          f" examples per size. This might take a while ...")
+          f" examples per size. Data will be stored in {data_folder}. This might take a while...")
     ErdosRenyiInMemoryDataset.create_dataset(data_folder, sizes, DEFAULT_EXAMPLES_PER_SIZE_IN_DATASET,
                                              HAMILTONIAN_PROBABILITY, solve_with_concorde=True)
     print(f"Successfully created Erdos-Renyi dataset with sizes {sizes} of {nr_examples} examples each.")
