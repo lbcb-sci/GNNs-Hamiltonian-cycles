905c7f23c872e2cf3439d53e128413da22252918
Filip Bosnic <fbosnic@fer.hr>
2022-07-14 16:30:35 +0200

    Corrected setup.py file and updated submodules

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="gnn-hamilton",
     version="0.1",
-    packages=[{"ham_gnn": "src"}],
-    install_requires=["torch", "torch_geometric", "json", "numpy"]
+    packages=find_packages(),
+    install_requires=["torch", "torch_geometric", "numpy"]
 )
