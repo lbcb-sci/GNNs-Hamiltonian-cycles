24c2b9ed1c1304c01f8ac369ca474cd35ea9cf88
slavica <fbosnic@fer.hr>
2022-11-08 11:25:48 +0000

    Added matplotlib plot for hamilton cycle accuracies

--- hamgnn/visualisation_tools.py
+++ hamgnn/visualisation_tools.py
@@ -1,3 +1,5 @@
+import pandas
+import seaborn
 import networkx as nx
 import torch_geometric as torch_g
 from matplotlib import pyplot as plt
@@ -32,3 +34,32 @@ def display_ER_graph_spring(d: torch_g.data):
     ax = fig.add_subplot(1, 1, 1)
     nx.draw(g, nx.spring_layout(g), ax=ax)
     return fig
+
+
+def display_accuracies(df: pandas, ax, colors=None, line_styles=None, fill_alpha=0.2):
+    _unique_sizes = sorted([s for s in df["size"].unique()])
+    ax.set_xlabel("Graph size (number of nodes)")
+    _x_min = 0
+    _x_max = max(_unique_sizes) * 1.1
+    ax.set_xlim(_x_min, _x_max)
+    ax.set_xticks(_unique_sizes)
+    ax.set_xticklabels(_unique_sizes)
+    ax.set_ylabel("Fraction of graphs solved (HC found)")
+    ax.set_ylim(0, 1)
+    _yticks = [0.1 * x for x in range(0, 11, 1)]
+    ax.set_yticks(_yticks)
+    ax.set_yticklabels([f"{x:.1f}" for x in _yticks])
+    ax.hlines(_yticks, _x_min, _x_max, color="black", linestyle="dotted", alpha=0.1)
+
+    for idx, (group_name, group) in enumerate(df.groupby("name")):
+        line_style = line_styles[idx]
+        color = colors[idx]
+        ax.plot(group["size"], group["perc_hamilton_found"], line_style, color=color, label=group_name)
+        if "confidence_delta" in group:
+            ax.fill_between(
+                group["size"],
+                group["perc_hamilton_found"] + group["confidence_delta"],
+                group["perc_hamilton_found"] - group["confidence_delta"],
+                color=color,
+                alpha=fill_alpha)
+    ax.legend()
