70d0a37e913a4a368f26f69cd50bdd0bb78a4fb0
slavica <fbosnic@fer.hr>
2022-11-17 17:20:21 +0000

    Added argparser in model training script

--- terminal_scripts/train_model_variation.py
+++ terminal_scripts/train_model_variation.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import sys
 
 import hamgnn.models_list as models_list
@@ -5,21 +6,22 @@ import hamgnn.model_utils as model_utils
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser(f"Trains one of the models defined in {models_list.__file__} specified by a name")
+    parser.add_argument("target", type=str, help="Name of the class of model to train")
+    parser.add_argument("--run-name", type=str, default=None, help="Gives a name to wandb run for this training experiment")
+    parser.add_argument("--gpu", type=int, default=None, help="Overrides gpu on which to train")
+    args = parser.parse_args()
+    target_name = args.target
+    run_name = args.run_name
+    gpu_id_override = args.gpu
+
     possible_targets = [var_name for var_name in dir(models_list) if isinstance(getattr(models_list, var_name, None), model_utils.ModelTrainRequest)]
-    args = sys.argv
-
-    run_name = None
-    assert len(args) <= 3
-    if len(args) == 2:
-        target_name = args[-1]
-    if len(args) == 3:
-        target_name = args[-2]
-        run_name = args[-1]
-    else:
-        target_name = None
-
     if target_name in possible_targets:
-        getattr(models_list, target_name).train(nr_cpu_threads=1, run_name=run_name)
+        train_request = getattr(models_list, target_name)
+        if gpu_id_override is not None:
+            train_request.arguments["trainer_hyperparams"]["gpus"] = [gpu_id_override]
+
+        train_request.train(nr_cpu_threads=1, run_name=run_name)
     else:
         print(f"Could not find model request {target_name} to train. Please use one of the following as an argument")
         for possible_target in possible_targets:
